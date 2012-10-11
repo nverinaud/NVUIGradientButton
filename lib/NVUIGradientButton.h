d3c4f5a92cf76664208186b803337fd2eb119332
Nicolas VERINAUD <nicolas@chlorophyll-vision.com>
2012-10-11 14:00:55 +0200

    [BugFix] Changing the style on the fly could sometimes cause a crash.

--- lib/NVUIGradientButton.h
+++ lib/NVUIGradientButton.h
@@ -8,7 +8,7 @@
 #import <UIKit/UIKit.h>
 
 typedef enum {
-	NVUIGradientButtonStyleDefault = 0,
+	NVUIGradientButtonStyleDefault = 1,
 	NVUIGradientButtonStyleBlackOpaque,
 	NVUIGradientButtonStyleBlackTranslucent
 } NVUIGradientButtonStyle;
